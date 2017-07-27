8deec551e968eaccdfc407355436d3a654ca3cba
marianajane <mariana.musm@gmail.com>
2017-07-27 17:11:51 -0500

    Ejercicio de pug backyard

---
+++ README.md
@@ -0,0 +1,3 @@
+# Backyard
+# Backyard
+# Backyard-pug
---
+++ gulpfile.js
@@ -0,0 +1,18 @@
+var gulp = require('gulp');
+var jade = require('gulp-jade');
+
+gulp.task('jade', function(){
+    gulp.src('./src/*.jade')
+    .pipe(jade({
+        pretty: true
+    }))
+    .pipe(gulp.dest('./dist'))
+})
+
+//instruccion para que actualice los cambios de .jade a .html (watch esta alerta a los cambios que se hacen)
+
+gulp.task('watch', function(){
+    gulp.watch('./src/*.jade', ['jade'])
+});
+
+gulp.task('default', ['jade', 'watch'])
